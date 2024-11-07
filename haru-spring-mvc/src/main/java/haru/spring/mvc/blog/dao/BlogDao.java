86decb5503e757a9eb9bfc7e318e33f196f77078
JY <JY@AngelMunggae>
2024-11-07 21:45:00 +0900

    feat: 자바 빈 설정, DAO 클래스 추가

---
+++ haru-spring-mvc/src/main/java/haru/spring/mvc/blog/dao/BlogDao.java
@@ -0,0 +1,22 @@
+package haru.spring.mvc.blog.dao;
+
+import java.util.Map;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BlogDao {
+	@Autowired
+	SqlSessionTemplate sqlSessionTemplate;
+	
+	public int insert(Map<String, Object> map) {
+	    int result = this.sqlSessionTemplate.insert("TB_BLG_CONT.insert", map);
+	    if (result > 0 && map.containsKey("seq_blg_cont")) {
+	        return (Integer) map.get("seq_blg_cont");
+	    }
+	 
+	    return -1;
+	}
+}
