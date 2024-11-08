2e9121c1859c4f8dfcbd773d45f1534bb10b44aa
jiyun-im-dev <jiyun.im.dev@gmail.com>
2024-11-08 17:47:40 +0900

    feat: 블로그 컨텐츠 작성 메서드 추가

--- haru-spring-mvc/src/main/java/haru/spring/mvc/blog/controller/BlogController.java
+++ haru-spring-mvc/src/main/java/haru/spring/mvc/blog/controller/BlogController.java
@@ -1,9 +1,12 @@
 package haru.spring.mvc.blog.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import haru.spring.mvc.blog.service.BlogService;
 
@@ -14,7 +17,13 @@ public class BlogController {
 	BlogService blogService;
 	
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
-	public String create() {
+	public String getCreate() {
 		return "blog/create";
+	}
+	
+	@RequestMapping(value = "/create", method = RequestMethod.POST)
+	public String postCreate(@RequestParam Map<String, Object> map) {
+	    int blogContSeq = this.blogService.create(map);
+	    return "redirect:/read/" + String.valueOf(blogContSeq);
 	}
 }
